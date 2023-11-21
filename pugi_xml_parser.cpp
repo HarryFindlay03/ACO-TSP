a6873ee0319920a3ad5560b3798fcbf986e4a714
Harry Findlay <harryfindlay@outlook.com>
2023-11-21 14:12:13 +0000

    generating distance matrix

--- pugi_xml_parser.cpp
+++ pugi_xml_parser.cpp
@@ -1,27 +1,62 @@
 #include <iostream>
 #include "pugixml.hpp"
 
+float** get_distance_matrix(char*, int);
+
 int main()
 {
+    int num_cities = 14;
+
+    char* filename = new char[sizeof("burma14.xml")];
+    strcpy(filename, "burma14.xml");
+
+    float** distance_matrix = get_distance_matrix(filename, num_cities);
+
+    int i, j;
+    for (i = 0; i < num_cities; i++)
+    {
+        for (j = 0; j < num_cities; j++)
+        {
+            std::cout << std::fixed;
+            std::cout.precision(0);
+            std::cout << distance_matrix[i][j] << "\t";
+        }
+        std::cout << std::endl;
+    }
+    return 0;
+}
+
+float** get_distance_matrix(char* filename, int num_cities)
+{
     pugi::xml_document doc;
-    if(!doc.load_file("burma14.xml")) return -1;
+    // need error handling here
+    doc.load_file(filename);
 
     pugi::xml_node nodes = doc.child("travellingSalesmanProblemInstance").child("graph");
-    std::cout << nodes.name() << std::endl;
 
-    for(pugi::xml_node node = nodes.first_child(); node; node = node.next_sibling())
+    int node_count, next_node_count;
+    pugi::xml_node node, cost;
+
+    float** distance_matrix = new float*[num_cities];
+
+    for(node = nodes.first_child(), node_count = 0; node; node_count++, node = node.next_sibling())
     {
-        std::cout << node.name() << std::endl;
-        for(pugi::xml_node cost = node.first_child(); cost; cost = cost.next_sibling())
+        distance_matrix[node_count] = new float[num_cities]; 
+        for(cost = node.first_child(), next_node_count = 0; cost; cost = cost.next_sibling(), next_node_count++)
         {
+            //ignores itself
+            if(node_count == next_node_count) next_node_count++;
+
+
             // converting to a double
             double d;
             const char* s = cost.attribute("cost").value();
             sscanf(s, "%lf", &d);
-            std::cout << d << std::endl;
+            distance_matrix[node_count][next_node_count] = d;
         }
     }
-    return 0;
+
+    return distance_matrix;
 }
 
 
